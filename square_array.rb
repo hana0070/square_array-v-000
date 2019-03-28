2d3e6b059956c1ccd9ae20b283512f92ef812e3e
HANA LEANDER <hana0070@gmail.com>
2019-03-28 13:33:01 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -3,12 +3,3 @@ def square_array(array)
   self.map {|array| array ** 2}
   array.square!
 end
-
-
-class Array
-  def square!
-    self.map! {|num| num ** 2}
-  end
-end
-
-[1,2,3].square! #=> [1, 4, 9]
